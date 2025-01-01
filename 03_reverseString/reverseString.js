91441cd74a607331a56e70417d46c92a9443b29f
Rustam <189756814+rustleium@users.noreply.github.com>
2025-01-01 11:49:24 +0530

    feat(reverseString): add string reversing logic
    - Added logic to reverse inputted string
    - Used split(), reverse(), and join() functions

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
+const reverseString = function(inputStr) {
 
+  let outputStr = inputStr.split("").reverse().join("");
+  return outputStr;
 };
 
 // Do not edit below this line
