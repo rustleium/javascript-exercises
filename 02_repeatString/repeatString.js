8dc7812bd98dacb2f0f48432a2fbde06795ebd7b
Rustam <189756814+rustleium@users.noreply.github.com>
2025-01-01 10:49:33 +0530

    feat(repeatString): add string repetition logic
    - Added logic to repeat a string 'num' times
    - Handles case where 'num' is negative by returning "ERROR"
    - Uses a loop to concatenate the string and build the output

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+  let outputString = "";
+  if(num < 0) {
+    return "ERROR";
+  } else {
+    for(let count = 1; count <= num; count++){
+      outputString += string;
+    }
+  }
+  return outputString;
 };
 
 // Do not edit below this line
