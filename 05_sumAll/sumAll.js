dd54398c8163663a1423d081cf7afc5c511ed00b
Rustam <189756814+rustleium@users.noreply.github.com>
2025-01-02 10:40:02 +0530

    feat(sumAll): Implement sumAll function
    - Calculates the sum of all numbers from int1 to int2 (inclusive).
    - Includes input validation:
      - Returns "ERROR" if either int1 or int2 is not an integer.
      - Returns "ERROR" if either int1 or int2 is negative.
    - Handles cases where int1 is less than or greater than int2.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function (int1, int2) {
+  let finalSum = 0;
+  if (!Number.isInteger(int1) || !Number.isInteger(int2)) return "ERROR";
+  if (int1 < 0 || int2 < 0) return "ERROR";
+  if ((int1 < int2)) {
+    for (int1; int1 <= int2; int1++) {
+      finalSum += int1;
+    }
+  } else {
+    for (int2; int2 <= int1; int2++) {
+      finalSum += int2;
+    }
+  }
+  return finalSum;
 };
 
 // Do not edit below this line
