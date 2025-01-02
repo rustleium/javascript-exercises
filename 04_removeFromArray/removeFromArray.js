5d56ba8a477911af9aad5459db6d941eb1c2e6c8
Rustam <189756814+rustleium@users.noreply.github.com>
2025-01-02 09:09:39 +0530

    feat(removeFromArray): logic to remove array item
    - Used for...of loop to compare the inputted array
      elements with the ...restParameters.
    - Only the elements present in the inputted array
      will be compared with the ...restParameters, so
      there will be no ERROR for non-present elements

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function (arr, ...elementsToRemove) {
 
+  let resultArray = [];
+  for (let element of arr) {
+
+    if (elementsToRemove.includes(element)) {
+      continue;
+    } else {
+      resultArray.push(element);
+    }
+  };
+  return resultArray;
+}
 // Do not edit below this line
 module.exports = removeFromArray;
