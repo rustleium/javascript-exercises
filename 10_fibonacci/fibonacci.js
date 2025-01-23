d0e89e15e3495197dc167065ed42d3a365641bc3
Rustam <189756814+rustleium@users.noreply.github.com>
2025-01-23 20:05:19 +0530

    feat: implement code to return nth fibonacci
    - Also enable string integer using parseInt()

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,33 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+  if(typeof n !== 'number') {
+    n = parseInt(n, 10);
+  }
 
+  if(n < 0) {
+    return "OOPS";
+  }
+
+  if(n === 0){
+    return 0;
+  }
+  
+  if(n === 1) {
+    return 1;
+  }
+
+  if( n <= 2) {
+    return 1;
+  }
+
+  let first = 1;
+  let second = 1;
+
+  for(let i = 2; i < n; i++) {
+    let next = first + second;
+    first = second;
+    second = next;
+  }
+  return second;
 };
 
 // Do not edit below this line
