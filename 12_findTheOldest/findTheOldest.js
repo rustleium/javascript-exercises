90a82b39fd6ec79c03f15786835aee4cf17d9340
Rustam <189756814+rustleium@users.noreply.github.com>
2025-01-25 08:37:08 +0530

    feat: find the oldest person using reduce

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  if(!people || people.length === 0) {
+    return null;
+  }
 
+  return people.reduce((oldest, current) => {
+    let currentAge = (current.yearOfDeath || new Date().getFullYear()) - current.yearOfBirth;
+    let oldestAge = (oldest.yearOfDeath || new Date().getFullYear()) - oldest.yearOfBirth;
+
+    return currentAge > oldestAge ? current : oldest;
+  });
 };
 
 // Do not edit below this line
